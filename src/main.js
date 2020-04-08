2c3bb9f102cec3c44e240d6358b92722a3fa28e2
Tennessee <tephilli@ucsc.edu>
2020-04-08 17:52:57 -0500

    almost working, no strafield

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
+    scene: [Menu, Play],
+
 };
 
 let game = new Phaser.Game(config);
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,4 +2,11 @@ class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
     }
+
+    create() {
+        //display menu text
+        this.add.text(20, 20, "This doth be a Rocket Patrol Menu");
+        //launches the next scne
+        this.scene.start("playScene");
+    }
 }
