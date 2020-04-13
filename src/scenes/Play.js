837647c242972552c92162ba3b0725d209ab81a5
Tennessee Ward <tephilli@ucsc.edu>
2020-04-13 10:53:19 -0500

    collision?

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -47,4 +47,16 @@ class Play extends Phaser.Scene {
         this.ship02.update();
         this.ship03.update();
     }
+
+    checkCollision(rocket, ship) {
+        // simple AABB checking
+        if (rocket.x < ship.x + ship.width && 
+            rocket.x + rocket.width > ship.x && 
+            rocket.y < ship.y + ship.height &&
+            rocket.height + rocket.y > ship. y) {
+                return true;
+        } else {
+            return false;
+        }
+    }
 }
